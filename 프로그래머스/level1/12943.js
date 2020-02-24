92614e93e82c22953862630a5b4ecb2ca6b2f18f
torang <torang@samsungfire.com>
2020-02-24 10:55:11 +0900

    알고리즘 업데이트

--- 프로그래머스/level1/12943.js
+++ 프로그래머스/level1/12943.js
@@ -1,3 +1,19 @@
+/* 
+
+    날짜 : 2020.02.24
+    난이도 : level 1
+    링크 : https://programmers.co.kr/learn/courses/30/lessons/12943
+    제목 : 콜라츠 추측
+    통과여부 : 통과 (100.0 / 100.0)
+    
+*/
+
+
+
+// solution 1 (100.0 / 100.0)
+
+
+
 function solution(num, count = 0) {
     if(num == 1){
         console.log(count);
@@ -8,5 +24,3 @@ function solution(num, count = 0) {
         solution(num * 3 + 1, count + 1);
     }
 }
-
-solution(6);
